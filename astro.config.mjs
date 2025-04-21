7c1ce946647afed85f9c01c68b03d75a3bb972a4
NIYOMUGISHA Obed <obed6@duck.com>
2025-04-21 18:05:13 +0200

    Change favicon and site in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://blog-by-obed.vercel.app/',
   integrations: [sitemap()],
 });
